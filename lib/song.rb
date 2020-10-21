07c91d759ad4d10a542283f80f0ada22b739fb45
Kimberly Baker <bakerk2010@gmail.com>
2020-10-21 20:33:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
     @genre = genre
+    @artist = artist
     @@count += 1
     @@genres << genre
     @@artists << artist
