4a87c3320032b914648d0dd58d768ca8332da49c
Kimberly Baker <bakerk2010@gmail.com>
2020-10-21 20:06:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+
+  def initialize 
+    @@count += 1
+    @@genres += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  @@genres = []
+  
+  @@artists = []
+  
+end
